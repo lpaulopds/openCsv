1c37c11deac2396ad323464eabd31a7d16a70b54
Luiz Paulo <luizpaulopds@outlook.com.br>
2021-11-18 12:21:21 -0300

    Lendo arquivo csv com fgetcsv do PHP

---
+++ MauticSurvey.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Mautic User Survey Results 2021 - Redacted</title>
+</head>
+<body>
+    <?php $filename = fopen("MauticSurvey2021.csv", "r") ?>
+    <?php $header = fgetcsv($filename, 0, ",") ?>
+    <?php $countRows = count($header) ?>
+
+    <h3>Mautic User Survey Results 2021 - Redacted</h3>
+    <p>Total Users: <b><?= $countRows ?></b></p>
+
+    <table>
+        <thead>
+            <tr>
+                <?php foreach ($header as $headerColumn): ?>
+                    <th><?= $headerColumn ?></th>
+                <?php endforeach ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($row = fgetcsv($filename, 0, ",")): ?>
+                <tr>
+                    <?php foreach ($row as $rowColumn): ?>
+                        <td><?= $rowColumn ?></td>
+                    <?php endforeach ?>
+                </tr>
+            <?php endwhile ?>
+        </tbody>
+    </table>
+</body>
+</html>
